79d68d84dc208f34b60863f03ad3a82a5d0c1c15
mrmuli <jayjaymuli00@gmail.com>
2018-08-25 18:33:37 +0300

    added read_data method
    parse input

--- todo.py
+++ todo.py
@@ -3,6 +3,14 @@
 class todo(object):
     def init(self):
         pass
+    
+    def read_data(self, input):
+        return "get some input from x"
+    
+
+# helper method
+def parse_input(input):
+    return "parse input for the read_data method"
 
 def main():
     tdo = todo()
